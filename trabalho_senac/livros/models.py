12b30ca943c0496e4a0e1aa4544b85e6a390edb3
ApolloNeves2008 <apolloclaudeir@gmail.com>
2025-02-07 19:04:58 -0300

    3.0
    admin, settings e models feitos

--- trabalho_senac/livros/models.py
+++ trabalho_senac/livros/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class livro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=100)
+    ano_publicacao = models.IntegerField()
+    genero = models.CharField(max_length=50)
+    descricao = models.TextField(blank=True)
+    disponivel = models.BooleanField(default=True)  
+
+    def __str__(self):
+        return self.titulo
+    
+
--- trabalho_senac/setup/settings.py
+++ trabalho_senac/setup/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'livros.apps.LivrosConfig',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
